7088db7999cc48ebc3ac529a9f09a507ebd0c7cf
Daniel Pucciariello <daniel.pucciariello@gmail.com>
2013-07-31 21:54:59 -0300

    Primeiro Commit

---
+++ eventex/core/tests.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from django.test import TestCase
+
+
+class HomepageTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_get(self):
+        """
+        Get / must return status code 200.
+        """
+        self.assertEqual(200, self.resp.status_code)
+
+
+    def test_template(self):
+        """
+        Homepage must use template index.html
+        """
+        self.assertTemplateUsed(self.resp, 'index.html')
